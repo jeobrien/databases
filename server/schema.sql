c0358a58f40b2edc44a9fa4d518e5e30663564ea
jeobrien <j.e.obrien28@gmail.com>
2015-10-30 15:48:55 -0700

    Set up schema.sql and client files

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,28 @@
 CREATE DATABASE chat;
 
 USE chat;
+CREATE TABLE rooms (
+  room_id int NOT NULL AUTO_INCREMENT,
+  room_name varchar(20),
+  PRIMARY KEY(room_id)
+);
+
+CREATE TABLE users (
+  user_id int NOT NULL AUTO_INCREMENT,
+  user_name varchar(20),
+  PRIMARY KEY(user_id)
+);
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  message_id int(5) NOT NULL AUTO_INCREMENT,
+  message_text text(250),
+  created datetime,
+  room_id_Rooms int,
+  user_id_Users int,
+  PRIMARY KEY(message_id),
+  FOREIGN KEY(room_id_Rooms) REFERENCES rooms(room_id),
+  FOREIGN KEY(user_id_Users) REFERENCES users(user_id)
 );
 
 /* Create other tables and define schemas for them here! */
